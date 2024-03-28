796f7388ed469d72282ac1b2530db0f04ef6606e
Nathan Peters <peters.nate8@gmail.com>
2024-03-28 12:03:25 -0600

    add validations for user entries and set up user create method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    # validations
+    validates :username, presence: true, length: { minimum: 3, maximum: 20 }
+    validates :password, presence: true, length: { minimum: 8, maximum: 20 }
+    validates_uniqueness_of :username
 end
