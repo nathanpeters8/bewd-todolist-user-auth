85847171c8bd8a09b610ffcdd2402659b8d4b2eb
Nathan Peters <peters.nate8@gmail.com>
2024-03-28 11:54:35 -0600

    add username and password columns to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_03_28_174828) do
+ActiveRecord::Schema.define(version: 2024_03_28_175212) do
 
   create_table "tasks", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2024_03_28_174828) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.string "password"
   end
 
 end
