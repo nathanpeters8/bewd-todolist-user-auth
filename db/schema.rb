4672f771a467b3f14a94047d1bec4c5dd1f0ae77
Nathan Peters <peters.nate8@gmail.com>
2024-03-28 11:50:20 -0600

    create user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2016_12_15_094136) do
-  create_table 'tasks', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'content'
-    t.boolean 'completed', default: false
+ActiveRecord::Schema.define(version: 2024_03_28_174828) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "content"
+    t.boolean "completed", default: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
 end
