0c9bbef7ed53e4c388b3af6478e09c7a315e5409
Nathan Peters <peters.nate8@gmail.com>
2024-03-28 12:20:25 -0600

    configure session and user models, generate token for every new session

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,2 +1,14 @@
 class Session < ApplicationRecord
+    belongs_to :user
+
+    # generate token for new session
+    before_validation :generate_session_token
+
+
+    private
+
+    def generate_session_token
+        #urlsafe_base64 generates a random URL-safe base64 string
+        self.token = SecureRandom.urlsafe_base64
+    end
 end
